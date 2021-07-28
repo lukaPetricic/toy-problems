2c8117de24d2d3306c927b64d6c9c8039b2e2bfb
Luka Petricic <lukapetricic@gmail.com>
2021-07-28 10:51:54 -0700

    Completed longestCommonPrefix

--- longestCommonPrefix/longestCommonPrefix.js
+++ longestCommonPrefix/longestCommonPrefix.js
@@ -2,6 +2,28 @@
  * @param {string[]} strs
  * @return {string}
  */
- var longestCommonPrefix = function(strs) {
 
+var getCommonPrefix = function (str1, str2) {
+  var common = '';
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] === str2[i]) {
+      common += str1[i];
+    } else {
+      break;
+    }
+  }
+
+  return common;
+}
+
+var longestCommonPrefix = function (strs) {
+  var currentLongest = strs[0];
+
+  for (word of strs) {
+    currentLongest = getCommonPrefix(currentLongest, word)
+  }
+
+  return currentLongest;
 };
+
+// console.log(longestCommonPrefix(["dog","racecar","car"]))
