035d0acd7b996428b92dbf9bec306a3db4454191
Luka Petricic <lukapetricic@gmail.com>
2021-08-01 16:55:13 -0700

    Solved strStr implementation

--- strStr/strStr.js
+++ strStr/strStr.js
@@ -4,5 +4,19 @@
  * @return {number}
  */
  var strStr = function(haystack, needle) {
+  if (!needle) return 0;
 
+  for (var i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle[0]) {
+      for (var j = 0; j < needle.length; j++) {
+        if (haystack[i + j] !== needle[j]) break;
+        if (j === needle.length - 1) return i;
+      }
+    }
+  }
+
+  return -1;
 };
+
+// var result = strStr('babac', 'bac');
+// console.log(result);
