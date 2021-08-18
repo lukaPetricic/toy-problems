19bb9dd42c4cf279821f2179e60bea574254f52a
lukaPetricic <lukapetricic@gmail.com>
2021-08-18 10:33:02 -0700

    Solved generateParenthesis

--- generateParenthesis/generateParenthesis.js
+++ generateParenthesis/generateParenthesis.js
@@ -1,3 +1,19 @@
 function generateParenthesis (n) {
-    
+    let result = [];
+
+    function tryAll(opened, closed, string) {
+        if (string.length === n * 2) {
+            result.push(string);
+            return;
+        }
+
+         if (opened < n) tryAll(opened + 1, closed, string + '(')
+         if (opened > closed) tryAll(opened, closed + 1, string + ')')
+    }
+
+    tryAll(0, 0, '')
+
+    return result;
 };
+
+console.log(generateParenthesis(3))
