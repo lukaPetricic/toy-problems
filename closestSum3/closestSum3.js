3a37292ba912a73d4e0c914820ed19661cc19558
lukaPetricic <lukapetricic@gmail.com>
2021-08-30 17:07:54 -0700

    Solved with better bigO time

--- closestSum3/closestSum3.js
+++ closestSum3/closestSum3.js
@@ -1,24 +1,46 @@
-function threeSumClosest(nums, target) {
-    var closest = nums[0] + nums[1] + nums[2];
-    // iterate over the array
-    for (var i = 0; i < nums.length; i++) {
-        for (var j = i + 1; j < nums.length; j++) {
-            for (var k = j + 1; k < nums.length; k++) {
-                var sum = nums[i] + nums[j] + nums[k];
-                if (isCloser(sum, target, closest))
-                    closest = sum;
-            }
-        }
-        if (closest === target)
-            return closest;
-    }
-    return closest;
-}
-;
+// function threeSumClosest(nums: number[], target: number): number {
+//     let closest = nums[0] + nums[1] + nums[2];
+//     // iterate over the array
+//     for (let i = 0; i < nums.length; i++) {
+//         for(let j = i + 1; j < nums.length; j++) {
+//             for(let k = j + 1; k < nums.length; k++) {
+//                 let sum = nums[i] + nums[j] + nums[k];
+//                 if (isCloser(sum, target, closest)) closest = sum;
+//             }
+//         }
+//         if (closest === target) return closest;
+//     }
+//     return closest;
+// };
 function isCloser(sum, target, currentClosest) {
     if (Math.abs(sum - target) < Math.abs(currentClosest - target))
         return true;
     return false;
 }
+function threeSumClosest(nums, target) {
+    // sort the array
+    var closest = nums[0] + nums[1] + nums[2];
+    nums.sort(function (a, b) { return a - b; });
+    // loop through the array
+    for (var i = 0; i < nums.length - 2; i++) {
+        // move two pointers accordingly
+        var left = i + 1;
+        var right = nums.length - 1;
+        while (left < right) {
+            // check if the sum is closer
+            var sum = nums[i] + nums[left] + nums[right];
+            if (isCloser(sum, target, closest))
+                closest = sum;
+            if (sum > target) {
+                right--;
+            }
+            else {
+                left++;
+            }
+        }
+    }
+    return closest;
+}
+;
 console.log(threeSumClosest([-1, 2, 1, -4], 1) === 2);
 console.log(threeSumClosest([0, 0, 0], 1) === 0);
--- closestSum3/closestSum3.ts
+++ closestSum3/closestSum3.ts
@@ -1,24 +1,49 @@
-function threeSumClosest(nums: number[], target: number): number {
-    let closest = nums[0] + nums[1] + nums[2];
+// function threeSumClosest(nums: number[], target: number): number {
+//     let closest = nums[0] + nums[1] + nums[2];
 
-    // iterate over the array
-    for (let i = 0; i < nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
-            for(let k = j + 1; k < nums.length; k++) {
-                let sum = nums[i] + nums[j] + nums[k];
-                if (isCloser(sum, target, closest)) closest = sum;
-            }
-        }
-        if (closest === target) return closest;
-    }
+//     // iterate over the array
+//     for (let i = 0; i < nums.length; i++) {
+//         for(let j = i + 1; j < nums.length; j++) {
+//             for(let k = j + 1; k < nums.length; k++) {
+//                 let sum = nums[i] + nums[j] + nums[k];
+//                 if (isCloser(sum, target, closest)) closest = sum;
+//             }
+//         }
+//         if (closest === target) return closest;
+//     }
 
-    return closest;
-};
+//     return closest;
+// };
 
 function isCloser(sum: number, target: number, currentClosest: number): boolean {
     if (Math.abs(sum - target) < Math.abs(currentClosest - target)) return true;
     return false;
 }
+
+function threeSumClosest(nums: number[], target: number): number {
+    // sort the array
+    let closest = nums[0] + nums[1] + nums[2];
+    nums.sort((a, b) => a - b)
+    // loop through the array
+    for (let i = 0; i < nums.length - 2; i++) {
+        // move two pointers accordingly
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            // check if the sum is closer
+            let sum = nums[i] + nums[left] + nums[right];
+            if (isCloser(sum, target, closest)) closest = sum;
+
+            if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+    }
+
+    return closest;
+};
 
 console.log(threeSumClosest([-1, 2, 1, -4], 1) === 2)
 console.log(threeSumClosest([0, 0, 0], 1) === 0)
